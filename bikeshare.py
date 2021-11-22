d39e9cec230f2f6828a9637cc7b68f896847c6e3
Casper Damen <cdamen@bol.com>
2021-11-22 17:18:42 +0100

    refactor: explanation on user stats details
    Extend function description with dimension detail for user stats

--- bikeshare.py
+++ bikeshare.py
@@ -138,7 +138,7 @@ def trip_duration_stats(df):
 
 
 def user_stats(df):
-    """Displays statistics on bikeshare users."""
+    """Displays statistics on bikeshare users such as birh year and gender."""
 
     print('\nCalculating User Stats...\n')
     start_time = time.time()
